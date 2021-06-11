b02198ccd7cc5a199f2c95af2c857a0034d11241
yan.costa <yan.costa@projuris.com.br>
2021-06-11 11:52:05 -0300

    Alterando para retornar um endereço de acordo com o cpf informado.

--- src/main/java/com/projetinho/service/AddressService.java
+++ src/main/java/com/projetinho/service/AddressService.java
@@ -1,8 +1,14 @@
 package com.projetinho.service;
 
 import com.projetinho.controller.dto.AddressDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(url = "https://viacep.com.br/ws/", name = "viacep" )
 public interface AddressService {
 
-    AddressDTO create(AddressDTO addressDto);
+    @GetMapping("{cep}/json")
+    AddressDTO getCep(@PathVariable("cep") String cep);
+
 }
